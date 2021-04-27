74ffc86782ca435534a09ab2092c891f379c5458
NotSoToothless <67082114+FURRO404@users.noreply.github.com>
2021-04-27 14:10:56 -0700

    Update STTTS.py

--- STTTS.py
+++ STTTS.py
@@ -1,23 +1,56 @@
-#=============FURRO404=============#
-#STTTS.py
+#===========FURRO404===========#
+#TTS
 import speech_recognition as sr
 from gtts import gTTS
 from playsound import playsound
 import os
+import time
+#------------------#
 r = sr.Recognizer()
-#--------------------------------------------------#
+
+def Language_Selector():
+    global lang1
+    global lang2
+    lang_list = ["de-DE", "en-US"]
+    
+    print(" - - - Language Codes - - -")
+    print(" - - - - - - - - - - - - - - - - - -")
+    print(" - - German ~ de-DE - - ")
+    print(" - - English ~ en-US - - ")                  #Language Selector Section
+    lang1 = str(input("\nEnter language code: "))
+    
+    if lang1 not in lang_list:
+        print("\n\nINVALID LANGUAGE CODE\n\n")
+        Language_Selector()
+
+    else:
+        lang_conv = {
+            "de-DE" : "de",
+            "en-US" : "en"
+            }
+        lang2 = lang_conv[lang1]
+#-----------------Actual Engine-----------------#
+Language_Selector()
 while True:
-    start = input("Press enter to start: ")
+    ready = input("Press enter when ready: ")
     with sr.Microphone() as source:
         try:
+            print("\n\nListening!")
             audio = r.listen(source)
-            text = r.recognize_google(audio)
+            text = r.recognize_google(audio, language = lang1)   #Speech to Text
             print("You said:", text, "\n")
-            sentence = gTTS(text)
-            sentence.save('My_file.mp3')
-            playsound('My_file.mp3')
-            os.remove('My_file.mp3')
-        except:
-            print("whoops lol, I didnt get that")
 
-#=============FURRO404=============#
+            if text == "change language":
+                Language_Selector()
+                continue
+
+            sentence = gTTS(text, lang = lang2)
+            sentence = gTTS(text, lang = lang2)
+            sentence.save('My_file.mp3')
+            playsound('My_file.mp3')        #Text to Speech
+            os.remove('My_file.mp3')
+            
+        except:
+            print("whoops lol, I didnt get that\n")
+            time.sleep(1)
+#===========FURRO404===========#
